77abc14ed2a18f9aab78028fcf6000f339be2894
FiqieAstahta <fiqieulya@gmail.com>
2020-12-03 10:35:42 +0700

    add contants

---
+++ lib/constants.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class BasePalette {
+  static Color green        = Color.fromARGB(255, 69, 170, 74);
+  static Color grey         = Color.fromARGB(255, 242, 242, 242);
+  static Color grey200      = Color.fromARGB(200, 242, 242, 242);
+  static Color menuRide     = Color(0xffe99e1e);
+  static Color menuCar      = Color(0xff14639e);
+  static Color menuBluebird = Color(0xff2da5d9);
+  static Color menuFood     = Color(0xffec1d27);
+  static Color menuSend     = Color(0xff8dc53e);
+  static Color menuDeals    = Color(0xfff43f24);
+  static Color menuPulsa    = Color(0xff72d2a2);
+  static Color menuOther    = Color(0xffa6a6a6);
+  static Color menuShop     = Color(0xff0b945e);
+  static Color menuMart     = Color(0xff68a9e3);
+  static Color menuTix      = Color(0xffe86f16);
+  static Color base         = Color(0xffd4423a);
+  static Color base_light   = Color(0xfff4a497);
+  static Color bg           = Color(0xFFFFFFFF);
+  static Color primary      = Color(0xFF7C6FB0);
+  static Color primaryMedium= Color(0xFFB0A8CF);
+  static Color primaryLight = Color(0xFFCAC5DF);
+  static Color accent       = Color(0xFF9370DB);
+  static Color accentMedium = Color(0xFF758ABB);
+  static Color accentLight  = Color(0xFFB8C2DC);
+  static Color secondary    = Color(0xFFF7EE6B);
+
+
+}
+
+class BaseString{
+  //static String base_url    = "http://119.2.50.170:9095/infocovid/index.php/services/";
+  static String base_url    = "https://yourfitpolarism.id/apimobile/";
+}
